e072b6ffd457f369cac8183dede12fb2fcfcdba1
Spandana-sp <SpandanaSP@kssem.edu.in>
2026-04-01 11:41:01 +0530

    Fix deployment status message typo

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def get_posts():
     response = requests.request("GET", url, headers=headers, data=payload)
     return jsonify({
         "data": response.json(),
-        "status": "Successfully triggered auto deployment BY SPANDANA",
+        "status": "Successfully triggered auto deployment BY SPANDU",
         "status_code": 500
     })
 
