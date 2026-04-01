34163252eab618f5b29367f6fecd5dd0ea32bf43
Spandana-sp <SpandanaSP@kssem.edu.in>
2026-04-01 11:24:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def get_posts():
     response = requests.request("GET", url, headers=headers, data=payload)
     return jsonify({
         "data": response.json(),
-        "status": "Spandana",
+        "status": "Successfully triggered auto deployment",
         "status_code": 500
     })
 
